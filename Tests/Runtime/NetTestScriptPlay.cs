86bf5d31e6038f5cc3ffa6d42e6daab2b214a456
Shuji Oba <shuji365630@gmail.com>
2021-07-07 00:03:09 +0900

    Add test file

---
+++ Tests/Runtime/NetTestScriptPlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NetTestScriptPlay
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NetTestScriptPlaySimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NetTestScriptPlayWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
